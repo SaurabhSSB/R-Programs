48424436c3b31f37c9e6b059dbbf0204f46de861
SaurabhSSB <saurabhsinghbhandarissb@gmail.com>
2024-10-14 00:35:57 +0530

    Update Sum, Mean and Product
    Updating
    
    Co-Authored-By: surbhi-2903 <99617379+surbhi-2903@users.noreply.github.com>

--- Sum, Mean and Product of the Vector.R
+++ Sum, Mean and Product of the Vector.R
@@ -1,24 +1,15 @@
 a<- c(12, 14, 16, 17)
-b<- c(18, 12, 11, 10)
 
 c<- sum(a)
-d<- sum(b)
 
 cat(" Sum of a is:- ", c)
-cat(" Sum of b is:- ", c)
-
 
 e<- length(a)
-f<- length(b)
 
 g<- c/e
-h<- d/f
 
 cat("Mean of a is:- ", g)
-cat("Mean of b is:- ", h)
 
 i<- prod(a)
-j<- prod(b)
 
 cat("Product of a is:- ",i)
-cat("Product of b is:- ",j) 
