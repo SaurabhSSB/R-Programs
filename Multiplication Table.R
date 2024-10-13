f8884106a8bfb2d1968d267f2800d899928fa9ee
SaurabhSSB <saurabhsinghbhandarissb@gmail.com>
2024-10-14 00:30:10 +0530

    Update Multiplication Table.R

--- Multiplication Table.R
+++ Multiplication Table.R
@@ -10,12 +10,5 @@ n <- 10
 # Create a matrix to store the multiplication table
 multiplication_table <- matrix(1:(n^2), nrow = n, ncol = n)
 
-# Fill the matrix with the product of row and column indices
-for (i in 1:n) {
-  for (j in 1:n) {
-    multiplication_table[i, j] <- i * j
-  }
-}
-
 # Print the multiplication table
 print(multiplication_table)
