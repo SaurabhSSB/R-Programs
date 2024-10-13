aa6aaf6e78c340b5b75dd1b8244d1465f96fa34c
SaurabhSSB <saurabhsinghbhandarissb@gmail.com>
2024-10-14 01:28:57 +0530

    Adding Sum of Vectors
    Sum of Vectors

--- Sum, Mean and Product of the Vector.R
+++ Sum, Mean and Product of the Vector.R
@@ -1,12 +1,16 @@
 a<- c(12, 14, 16, 17)
+b<- c(19, 18, 15, 13)
 
+# Sum of Vectors
 c<- sum(a)
+d<- sum(b)
 
 cat(" Sum of a is:- ", c)
+cat(" Sum of b is:- ",d)
 
 e<- length(a)
+f<- length(b)
 
 g<- c/e
 
 cat("Mean of a is:- ", g)
-
