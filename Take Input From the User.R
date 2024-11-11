70216d5219337723541000ae5a2d8fb7ff44a0b5
SaurabhSSB <saurabhsinghbhandarissb@gmail.com>
2024-11-12 00:20:03 +0530

    Updating Content

--- Take Input From the User.R
+++ Take Input From the User.R
@@ -5,13 +5,11 @@ print(paste("Hi",a))
 
 b<-readline("Enter Age:- ")
 cat("Your age is",b)
+
 c<-scan()
 print(c)
 
 a=7/3
-
-
-
 print(a, digit= 4)
 
 x <- matrix(c(2, NA, 5, NA, 8, NA, 22, 67, 43), 
