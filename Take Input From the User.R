c854a123e94deabb66c7522556c5c71e2e50acc7
SaurabhSSB <saurabhsinghbhandarissb@gmail.com>
2024-12-10 07:07:40 +0530

    Updating Content

--- Take Input From the User.R
+++ Take Input From the User.R
@@ -8,12 +8,12 @@ cat("Your age is",b)
 
 c<-scan()
 print(c)
-
 a=7/3
+
 print(a, digit= 4)
+
 x <- matrix(c(2, NA, 5, NA, 8, NA, 22, 67, 43), 
             nrow = 3, byrow = TRUE) 
-
 
 # na.print: indicates NA values output format
 
