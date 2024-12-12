56972af15a567d5ec13b6450a42c7b90feea83a8
SaurabhSSB <saurabhsinghbhandarissb@gmail.com>
2024-12-12 19:55:06 +0530

    Updating Content

--- Take Input From the User.R
+++ Take Input From the User.R
@@ -2,13 +2,12 @@
 
 a<- readline("Enter Name:- ")
 print(paste("Hi",a))
- b<-readline("Enter Age:- ")
+ 
+b<-readline("Enter Age:- ")
 cat("Your age is",b)
 
 c<-scan()
 print(c)
-
-
 
 a=7/3
 print(a, digit= 4)
